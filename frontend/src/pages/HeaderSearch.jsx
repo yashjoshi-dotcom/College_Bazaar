52fcc638f493d42c3d80ae30da856bf26257af91
Tanush15 <106920983+Tanush15@users.noreply.github.com>
2022-08-27 15:39:19 +0530

    created landing page and home page

---
+++ frontend/src/pages/HeaderSearch.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+// import image from "../HouseData/assets/img/house-banner.png"
+
+const image5 = require("../assets/img/search.ico");
+
+const HeaderSearch = () => {
+  return (
+    <div class="">
+      <div className="  flex justify-center  bg-[url('https://source.unsplash.com/1600x400/?home')] bg-cover  ">
+        <div className="flex justify-center w-4/5">
+          <input type="text" className="my-36 w-9/12  sm:h-24 rounded-l-xl px-4 outline-none " placeholder='Search flats here...' />
+          <button className=' my-36 sm:h-24 w-20 rounded-l-none rounded-r-xl  px-4 bg-sky-600  border-0 hover:bg-sky-700'><img className=' object-contain py-2 w-10' src={image5} alt="" /></button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeaderSearch 
