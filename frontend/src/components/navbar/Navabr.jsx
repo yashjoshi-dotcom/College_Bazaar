a75d539b3cfd45ccc6cab2d843e5b4b940b44dc4
Yuvi <81169447+YUVRAJg37@users.noreply.github.com>
2022-08-27 18:16:15 +0530

    Update Navabr.jsx

--- frontend/src/components/navbar/Navabr.jsx
+++ frontend/src/components/navbar/Navabr.jsx
@@ -8,7 +8,7 @@ const Navabr = () => {
   return (
     <div className="dark">
       <nav className="bg-white border-gray-200 px-2 pt-0 pb-0 sm:px-4 py-2.5 sm:pt-2 sm:pb-2  dark:bg-gray-900">
-        <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <div className="container flex flex-wrap justify-between items-center ">
           <a href="https://flowbite.com/" className="flex items-center text-xl">
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
               College-Bazzar
@@ -17,7 +17,7 @@ const Navabr = () => {
           <div className="flex md:order-2">
             <div className="hidden md:block">
               <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 ">
-                <li className=" mt-auto mb-auto">
+                <li className=" my-auto">
                   <a
                     href="#"
                     className="block py-2 pr-4 pl-3 text-base text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
