11f84b961d9d614040c7e05ace725d764e8e4dc4
yashjoshi-dotcom <77540753+yashjoshi-dotcom@users.noreply.github.com>
2022-08-28 18:17:58 +0530

    small changes in nav and backend

---
+++ backend/src/middlewares/Jwt_authenticate.js
@@ -0,0 +1,26 @@
+const jwt =require('jsonwebtoken');
+const User= require('../models/userSchema');
+require('dotenv').config({ path: 'config.env' });
+
+const Jwt_authenticate=async (req,res,next)=>{
+    try
+    {
+        const token=req.cookies.jwtoken;
+        const verifyToken=jwt.verify(token,process.env.JWT_KEY);
+        const rootUser =await User.findOne({_id: verifyToken._id,"tokens.token":token})
+        if(!rootUser)
+        {
+            throw new Error('User not found');
+        }
+        req.token=token;
+        req.rootUser=rootUser;
+        req.userID=rootUser._id;
+        next();
+    }
+    catch(err){
+        res.status(401).send('Unauthorised: NO token provided');
+        console.log(err);
+
+    }
+}
+module.exports=Jwt_authenticate;
