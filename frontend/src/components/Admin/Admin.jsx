ccf94a0ef280af7e19281879d4ea7c088e0d2b4e
Yuvi <81169447+YUVRAJg37@users.noreply.github.com>
2022-08-28 07:32:45 +0530

    Added Admin Page

--- frontend/src/components/Admin/Admin.jsx
+++ frontend/src/components/Admin/Admin.jsx
@@ -3,9 +3,11 @@ import Cards from "./AdminCards";
 
 const Admin = () => {
   return (
-    <div className="w-full h-[90rem]">
-      <div className="container px-64 pt-10 justify-center">
+    <div className="w-full h-[90rem] ">
+      <div className="flex flex-col m-4">
         <Cards />
+        <Cards productName="ABC" productInfo="hoidajdpk[adkpad,;akfp" />
+        <Cards productName="ABC" productInfo="hoidajdpk[adkpad,;akfp" />
       </div>
     </div>
   );
