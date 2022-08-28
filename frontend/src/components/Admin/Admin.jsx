025cc1e0ad573ef5792aecaa0e2645ee9899019b
Yuvi <81169447+YUVRAJg37@users.noreply.github.com>
2022-08-28 06:43:10 +0530

    form skeleton

---
+++ frontend/src/components/Admin/Admin.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Cards from "./AdminCards";
+
+const Admin = () => {
+  return (
+    <div className="w-full h-[90rem]">
+      <div className="container px-64 pt-10 justify-center">
+        <Cards />
+      </div>
+    </div>
+  );
+};
+
+export default Admin;
---
+++ frontend/src/components/Admin/AdminCards.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import img from "../../assets/img/handbag.jpg";
+
+const AdminCards = () => {
+  return (
+    <a
+      href="#"
+      className="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+    >
+      <img
+        className="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        src={img}
+        alt=""
+      />
+      <div className="flex flex-col  p-4 leading-normal">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Noteworthy technology acquisitions 2021
+        </h5>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          Here are the biggest enterprise technology acquisitions of 2021 so
+          far, in reverse chronological order.
+        </p>
+      </div>
+    </a>
+  );
+};
+
+export default AdminCards;
