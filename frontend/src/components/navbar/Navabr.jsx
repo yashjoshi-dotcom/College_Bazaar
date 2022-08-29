32ade8ed857b2acbade984f1d0771f5d3713f5b4
Yuvi <81169447+YUVRAJg37@users.noreply.github.com>
2022-08-29 11:36:14 +0530

    Squashed commit of the following:
    commit 5ee856503dd09d33cf400d2bbb9f845f10688c0e
    Author: Tanush15 <106920983+Tanush15@users.noreply.github.com>
    Date:   Mon Aug 29 11:30:09 2022 +0530
    
        Update LandingPage.jsx
    
    commit 2e926aa88b513ece46adf0d072f93bc282c9b171
    Author: Yuvi <81169447+YUVRAJg37@users.noreply.github.com>
    Date:   Mon Aug 29 10:14:12 2022 +0530
    
        updated landing page
    
    commit ed51f43a8cb2575118e62325d05599cb9e918acd
    Author: Yuvi <81169447+YUVRAJg37@users.noreply.github.com>
    Date:   Mon Aug 29 09:17:44 2022 +0530
    
        updated landing page
    
    commit 52019bfa07e54b3c96027dce2c7817dafc79246b
    Author: Yuvi <81169447+YUVRAJg37@users.noreply.github.com>
    Date:   Mon Aug 29 00:26:14 2022 +0530
    
        updated svg
    
    commit 46465137438406ed9d22fdfef05e4af09512be3a
    Author: Yuvi <81169447+YUVRAJg37@users.noreply.github.com>
    Date:   Sun Aug 28 22:12:01 2022 +0530
    
        Update Navabr.jsx

--- frontend/src/components/navbar/Navabr.jsx
+++ frontend/src/components/navbar/Navabr.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 import { useState } from "react";
 // import Menu from "./Menu";
-import 'flowbite';
+import "flowbite";
 import { Navbar } from "flowbite-react/lib/esm/components/Navbar";
 import { Dropdown } from "flowbite-react/lib/esm/components/Dropdown";
 import { Avatar } from "flowbite-react/lib/esm/components/Avatar";
@@ -11,76 +11,83 @@ import { Avatar } from "flowbite-react/lib/esm/components/Avatar";
 const Navabr = () => {
   // const [isMenuOpen, SetMenu] = useState(false);
   return (
-
     <div className="dark ">
-    <Navbar 
-    fluid={true}
-    rounded={true}
-  ><div className=" sm:absolute">
-    <Navbar.Brand   href="https://flowbite.com/">
-      <img
-        src="https://flowbite.com/docs/images/logo.svg"
-        className="mr-0 h-6 sm:h-9"
-        alt="Flowbite Logo"
-      />
-      <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-        Flowbite
-      </span>
-    </Navbar.Brand></div>
-    <div className="flex md:order-2 ">
-      <Dropdown
-        arrowIcon={false}
-        inline={true}
-        label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded={true}/>}
-      >
-        <Dropdown.Header>
-          <span className="block text-sm">
-            Bonnie Green
-          </span>
-          <span className="block truncate text-sm font-medium">
-            name@flowbite.com
-          </span>
-        </Dropdown.Header>
-        <Dropdown.Item>
-          Dashboard
-        </Dropdown.Item>
-        <Dropdown.Item>
-          Settings
-        </Dropdown.Item>
-        <Dropdown.Item>
-          Earnings
-        </Dropdown.Item>
-        <Dropdown.Divider />
-        <Dropdown.Item>
-          Sign out
-        </Dropdown.Item>
-      </Dropdown>
-      <Navbar.Toggle />
+      <Navbar fluid={true} rounded={true}>
+        <div className=" sm:absolute">
+          <Navbar.Brand href="https://flowbite.com/">
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="mr-0 h-6 sm:h-9"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+              Flowbite
+            </span>
+          </Navbar.Brand>
+        </div>
+        <div className="flex md:order-2 ">
+          <Dropdown
+            arrowIcon={false}
+            inline={true}
+            label={
+              <Avatar
+                alt="User settings"
+                img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                rounded={true}
+              />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">Bonnie Green</span>
+              <span className="block truncate text-sm font-medium">
+                name@flowbite.com
+              </span>
+            </Dropdown.Header>
+            <Dropdown.Item>Dashboard</Dropdown.Item>
+            <Dropdown.Item>Settings</Dropdown.Item>
+            <Dropdown.Item>Earnings</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
+          <Navbar.Toggle />
+        </div>
+        <div className="m-auto">
+          <Navbar.Collapse>
+            <NavLink
+              to="/Home"
+              active={true}
+              className=" text-lg text-neutral-400 hover:text-neutral-200"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/aboutus"
+              className="text-lg text-neutral-400 hover:text-neutral-200"
+            >
+              About Us
+            </NavLink>
+            <NavLink
+              to="/testimonials"
+              className="text-lg text-neutral-400 hover:text-neutral-200"
+            >
+              Testimonials
+            </NavLink>
+            <NavLink
+              to="/form"
+              className="text-lg text-neutral-400 hover:text-neutral-200"
+            >
+              List Your Items
+            </NavLink>
+            <NavLink
+              to="/contact"
+              className="text-lg text-neutral-400 hover:text-neutral-200"
+            >
+              Contact
+            </NavLink>
+          </Navbar.Collapse>
+        </div>
+      </Navbar>
     </div>
-    <Navbar.Collapse>
-      <NavLink
-        to="/Home"
-        active={true} className="text-neutral-400 hover:text-neutral-200" 
-      >
-        Home
-      </NavLink>
-      <NavLink to="/aboutus" className="text-neutral-400 hover:text-neutral-200">
-        About Us
-      </NavLink>
-      <NavLink to="/testimonials" className="text-neutral-400 hover:text-neutral-200">
-        Testimonials
-      </NavLink>
-      <NavLink to="/form" className="text-neutral-400 hover:text-neutral-200">
-        List Your Items
-      </NavLink>
-      <NavLink to="/contact" className="text-neutral-400 hover:text-neutral-200">
-        Contact
-      </NavLink>
-    </Navbar.Collapse>
-  </Navbar>
-  </div>
-// djsfoxdjokpg[f][]
-   
   );
 };
 
