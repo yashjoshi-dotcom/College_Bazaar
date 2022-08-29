6e36c2d263b6cc7ffc2d423ca0d29a2b2e0780bd
Tanush15 <106920983+Tanush15@users.noreply.github.com>
2022-08-29 12:18:14 +0530

    home page edited

--- frontend/src/pages/HeaderSearch.jsx
+++ frontend/src/pages/HeaderSearch.jsx
@@ -8,8 +8,8 @@ const HeaderSearch = () => {
     <div class="">
       <div className="  flex justify-center  bg-[url('https://source.unsplash.com/1600x400/?home')] bg-cover  ">
         <div className="flex justify-center w-4/5">
-          <input type="text" className="my-36 w-9/12  sm:h-24 rounded-l-xl px-4 outline-none " placeholder='Search flats here...' />
-          <button className=' my-36 sm:h-24 w-20 rounded-l-none rounded-r-xl  px-4 bg-sky-600  border-0 hover:bg-sky-700'><img className=' object-contain py-2 w-10' src={image5} alt="" /></button>
+          <input type="text" className="my-36 w-9/12  sm:h-20 rounded-l-xl px-4 outline-none " placeholder='Search flats here...' />
+          <button className=' my-36 sm:h-20 w-20 rounded-l-none rounded-r-xl  px-4 bg-sky-600  border-0 hover:bg-sky-700'><img className=' object-contain py-2 w-10' src={image5} alt="" /></button>
         </div>
       </div>
     </div>
