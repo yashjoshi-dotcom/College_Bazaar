cc6745fc5dea416a2fc8b242aec231e62050a80e
yashjoshi-dotcom <77540753+yashjoshi-dotcom@users.noreply.github.com>
2022-10-27 13:26:52 +0530

    Updatedd HTTP Response Status Codes
    Added HTTP Response code in a few missing places.
    Updated Response Messages accordingly.
    Reformated the overall structure of the file.

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+// Importing required libraries 
 const express = require("express");
 require("dotenv").config();
 const app = express();
@@ -18,23 +19,16 @@ const io = require("socket.io")(server, {
 });
 
 io.on("connection", (socket) => {
-  //console.log(`User Connected: ${socket.id}`);
-
   socket.on("join_room", (data) => {
     socket.join(data);
-    //console.log(`User With ID: ${socket.id} joined room : ${data}`);
   });
   socket.on("send_message", (data) => {
     socket.to(data.room).emit("receive_message", data);
   });
   socket.on("disconnect", () => {
-    //console.log("User Disconnected", socket.id);
   });
 });
 
-// server.listen(3001, () => {
-//   console.log("Server Running");
-// });
 
 const fileUpload = require("express-fileupload");
 app.use(
@@ -47,7 +41,7 @@ require("./src/db/conn");
 const ranks = require("./src/models/userSchema");
 app.use(express.json());
 app.use(require("./src/router/auth"));
-//
+
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("frontend/build"));
 }
--- backend/src/router/auth.js
+++ backend/src/router/auth.js
@@ -1,4 +1,4 @@
-// Importing essential libraries 
+// Importing required libraries 
 const express =require('express');
 const User= require('../models/userSchema');
 const bcrypt =require('bcryptjs');
@@ -15,20 +15,20 @@ const router=express.Router();
     // Checking if all data has been recieved on the backend.
     if(!name|| !email_id || !password)
     {
-        return res.status(409).json({error :"Plz enter all data.."});
+        return res.status(409).json({error :"Bad Request:{Conflict with server db schema} Plz enter all data.."});
     }
     //Checking if the Email-ID has @dtu.ac.in domain name
     var domain = email_id.substring(email_id.lastIndexOf("@") +1);
     if(domain!='dtu.ac.in')
     {
-        return res.status(400).json({error :"Only DTU institutional email ID's allowed."});
+        return res.status(400).json({error :"Bad Request: Only DTU institutional email ID's allowed."});
     }
 
     try
     {
         //Checking if Email Id already exists in the dB and if it exists returning message back 
         const userLogin = await User.findOne({email_id});
-        if(userLogin) return res.json({message:"Email already exists"});
+        if(userLogin) return res.status(403).json({error:"{Forbidden to create multiple accounts} Email already exists"});
 
                 
         const user =new User({name,email_id,college_name,password});
@@ -36,15 +36,16 @@ const router=express.Router();
         //Callling save method to add the data to the database 
         await user.save();
         console.log(user);
-        res.status(201).json({message :"registered sucessfully"});
+        res.status(201).json({message :"Registered sucessfully"});
     }
-    //Consoling Error for proper Debugging 
     catch(err){
+        //Consoling error for proper debugging.
         console.log(err);
+        res.status(500).json({error:"We are experiencing some server problems!!"})
     }
  });
 
- //Get the data of an indiviual
+ //Get the data of an indiviual by his Object_id 
 router.get('/register/:id',async (req,res)=>{
 
     try
@@ -52,48 +53,16 @@ router.get('/register/:id',async (req,res)=>{
         const _id= req.params.id;
         const single_user =await User.findById(_id);
         console.log(single_user);
-        res.status(201).send(single_user);
+        res.status(200).send(single_user);
     }
     catch(err){
+        //Consoling error for proper debugging.
         console.log(err);
+        res.status(500).json({error:"We are experiencing some server problems!!"})
     }
  });
 
- //Get the whole data from the database 
- router.get('/db',async (req,res)=>
- {
-    try
-    {
-        const user =await User.find({});
-        console.log(user);
-        res.status(201).send(user);
-    }
-    //Consoling Error for proper Debugging 
-    catch(err){
-        console.log(err);
-    }
- });
-
-
-
-  //The below function will be used to delete data of an indiviual.
-  router.delete('/register/:id',async (req,res)=>{
-
-    try
-    {
-        const _id= req.params.id;
-        const single_user =await User.findByIdAndDelete(_id);
-        console.log(single_user);
-        res.status(201).send(single_user);
-    }
-    catch(err){
-        res.status(500).send(e);
-        console.log(err);
-    }
- });
-//All above mentioned code is for registering a new user.
-
-// The below mentioned code is for loging in.
+ // The below mentioned function is responisble for allow ing users to log in.
 router.post('/signin',async (req,res)=>{
 
     try{
@@ -115,55 +84,41 @@ router.post('/signin',async (req,res)=>{
             //The value above need to be provided in millisconds    
             httpOnly:true, //Else it will work only in secure
         });
+
+        //Cosnoling Data of User who has logged in for developer's convinience
         console.log(userLogin);
-        if(isMatched) res.json({message:"Login Successful"});
-        else res.json({message:"Invalid Credentials"});
+
+        if(isMatched) res.status(200).json({message:"Login Successful"});
+        else res.status(403).json({message:"Access Denied : Invalid Credentials"});
 
     } catch(err)
     {
         console.log(err);
+        res.status(500).json({error:"We are experiencing some server problems!!"})
     }
-})
-//form page 
-router.get('/profilec',Jwt_authenticate,(req,res)=>{
-    res.send(req.rootUser);
-})
+});
 
-module.exports=router;
-
-
-/*
-
-The Commented Code has just been written for reference for now and will be utilised in a later update to the Project.:bulb:
-
-
- 
- //Will handle patch request for an indiviual
- router.patch('/register/:id',async (req,res)=>{
+  //The below function will be used to delete data of an indiviual fron the database .
+  router.delete('/register/:id',async (req,res)=>{
 
     try
     {
         const _id= req.params.id;
-        const single_user =await User.findByIdAndUpdate(_id,req.body,{
-            new:true
-        });
+        const single_user =await User.findByIdAndDelete(_id);
+        //Logging the user that has been deleted for developer's convenience.
         console.log(single_user);
-        res.status(201).send(single_user);
+        res.status(200).json({message:"Successfully removed the user from the database."});
     }
     catch(err){
-        res.status(500).send(e);
         console.log(err);
+        res.status(500).json({error:"We are experiencing some server problems!!"})
     }
  });
 
- //will patch listed data for an indiviual->>>
- router.patch('/add_data/:id',async (req,res)=>{
+//The given function adds a new listed item into the database and update list array.
+router.patch('/add_data/:id',async (req,res)=>{
     try
     {
-        // console.log("<<<>>>>");
-        // console.log(req.body);
-        // console.log("<<<>>>>");
-
         const newItem = 
         {
             item_name:req.body.item_name,
@@ -186,15 +141,36 @@ The Commented Code has just been written for reference for now and will be utili
             },
           }
         )
-        console.log("final work complete");
-        res.status(201).json({message:"good work ultimate"});
+        res.status(201).json({message:"Listing successfully added on the website"});
         
     }
     catch(err){
-        res.status(550).send(err);
         console.log(err);
+        res.status(500).json({error:"We are experiencing some server problems!!"})
     }
  });
 
- */
+//form page to get profile details after verifying the JWT token using middle Ware
+router.get('/profilec',Jwt_authenticate,(req,res)=>{
+    res.status(200).send(req.rootUser);
+});
+
+ //Get the whole data from the database 
+ router.get('/db',async (req,res)=>
+ {
+    try
+    {
+        const user =await User.find({});
+        console.log(user);
+        res.status(200).send(user);
+    }
+    catch(err){
+        //Consoling error for proper debugging.
+        console.log(err);
+        res.status(500).json({error:"We are experiencing some server problems!!"})
+    }
+ });
+
+ module.exports=router;
+
 
