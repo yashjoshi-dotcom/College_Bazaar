4b4df47c12128cda18e77f80b30c035bd96fa02d
yashjoshi-dotcom <yashjoshimail@gmail.com>
2022-12-26 18:06:18 +0530

    Removed transfer of non-required Params

--- backend/src/routes/all_listings.js
+++ backend/src/routes/all_listings.js
@@ -8,7 +8,11 @@ const router = express.Router();
 // Get the whole data from the database
 router.get('/', async (req, res) => {
   try {
-    const user = await User.find({});
+    // const user = await User.find({});
+    const user = await User
+  .find({})
+  .select({ "list": 1});
+
     console.log(user);
     res.status(200).send(user);
   } catch (err) {
